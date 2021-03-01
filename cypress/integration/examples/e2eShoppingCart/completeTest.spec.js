dca5f938e4788555a14df2bcf8bb3d63eb0f4850
Uebriges <uebriges@gmx.at>
2021-03-01 13:31:33 +0100

    Add cypress and first unit tests

--- cypress/integration/examples/e2eShoppingCart/completeTest.spec.js
+++ cypress/integration/examples/e2eShoppingCart/completeTest.spec.js
@@ -1,6 +1,6 @@
 import { e2eAddToCartTest } from './addToCart.spec';
 import { e2eChangeQuantity } from './changeQuantity.spec.';
-import { e2eRemoveFromCart } from './removeFromCart';
+import { e2eRemoveFromCart } from './removeFromCart.spec';
 
 context('shoppingCart', () => {
   e2eAddToCartTest();

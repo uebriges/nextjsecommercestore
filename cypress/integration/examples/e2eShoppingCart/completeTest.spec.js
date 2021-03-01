3d6e4d67475bc077934e5ed4a478732c5b4651d7
Uebriges <uebriges@gmx.at>
2021-03-01 16:05:47 +0100

    Fix problem with ley migration

--- cypress/integration/examples/e2eShoppingCart/completeTest.spec.js
+++ cypress/integration/examples/e2eShoppingCart/completeTest.spec.js
@@ -2,8 +2,8 @@ import { e2eAddToCartTest } from './addToCart.spec';
 import { e2eChangeQuantity } from './changeQuantity.spec.';
 import { e2eRemoveFromCart } from './removeFromCart.spec';
 
-context('shoppingCart', () => {
-  e2eAddToCartTest();
-  e2eChangeQuantity();
-  e2eRemoveFromCart();
-});
+// context('shoppingCart', () => {
+e2eAddToCartTest();
+e2eChangeQuantity();
+e2eRemoveFromCart();
+// });
